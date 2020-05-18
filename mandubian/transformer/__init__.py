17090d0d762d84168bd20c445d1360f2fa2d1a78
jonathan wong <jonmcwong@gmail.com>
2020-05-18 06:32:20 +0100

    added code for mandubian

---
+++ mandubian/transformer/__init__.py
@@ -0,0 +1,25 @@
+# from mandubian import transformer
+
+# from .transformer import Constants
+# from .transformer import Modules
+# from .transformer import Layers
+# from .transformer import SubLayers
+# from .transformer import Models
+# from .transformer import Translator
+# from .transformer import Beam
+# from .transformer import Optim
+
+
+import mandubian.transformer.Constants  as Constants
+import mandubian.transformer.Modules as Modules
+import mandubian.transformer.Layers as Layers
+import mandubian.transformer.SubLayers as Sublayers
+import mandubian.transformer.Models as Models
+import mandubian.transformer.Translator as Translator
+import mandubian.transformer.Beam as Beam
+import mandubian.transformer.Optim as Optim
+
+# from .transformer import 
+
+__all__ = [Constants, Modules, Layers, Sublayers, Models, Translator, Beam, Optim
+]
