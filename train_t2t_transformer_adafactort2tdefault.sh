4198fc2b5ab4a0b73fe1ee53b84984e73aeccee3
jonanathan <jonmcwong@gmail.com>
2020-06-11 05:58:01 +0000

    added training scripts

---
+++ train_t2t_transformer_adafactort2tdefault.sh
@@ -0,0 +1,34 @@
+# transformer script with original mds params
+
+export PROBLEM=algorithmic_math_deepmind_all
+export MODEL=transformer
+export HPARAMS=transformer_tpu
+export TPU_NAME=t2t-adafactor-0	# different for each run
+export STORAGE_BUCKET=gs://mathsreasoning
+
+export DATA_DIR=${STORAGE_BUCKET}/t2t-data
+export TMP_DIR=${STORAGE_BUCKET}/t2t_datagen
+export TRAIN_DIR=${STORAGE_BUCKET}/t2t_train/$PROBLEM/$MODEL-$HPARAMS
+
+# all the correct directories already exist. no need to mkdir
+
+# Generate data
+t2t-datagen \
+  --data_dir=$DATA_DIR \
+  --tmp_dir=$TMP_DIR \
+  --problem=$PROBLEM
+
+# Train
+# *  If you run out of memory, add --hparams='batch_size=1024'.
+t2t-trainer \
+  --data_dir=$DATA_DIR \
+  --problem=$PROBLEM \
+  --model=$MODEL \
+  --hparams_set=$HPARAMS \
+  --hparams='' \
+  --output_dir=$TRAIN_DIR \
+  --use_tpu=True \
+  --cloud_tpu_name=${TPU_NAME} \
+  --train_steps=600000 \
+  --eval_steps=3 \
+  --save_checkpoints_secs=1800
