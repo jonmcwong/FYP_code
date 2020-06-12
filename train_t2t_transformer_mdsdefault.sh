8db3660628fb4db6a37edb521c89e471be719ba6
jonathan wong <jonmcwong@gmail.com>
2020-06-12 11:16:35 +0100

    added adam transformer test

--- train_t2t_transformer_mdsdefault.sh
+++ train_t2t_transformer_mdsdefault.sh
@@ -1,14 +1,31 @@
 # transformer script with original mds params
 
+# register the tpu you're gonna use
+export TPU_IP_ADDRESS=ip-address
+export XRT_TPU_CONFIG="tpu_worker;0;$TPU_IP_ADDRESS:8470"
+
 export PROBLEM=algorithmic_math_deepmind_all
 export MODEL=transformer
-export HPARAMS=transformer_tpu
+export HPARAMS_SET=transformer_tpu
+export OVERIDE_HPARAMS='batch_size=1024, \
+                        clip_grad_norm=0.1, \
+                        dropout=0.1, \
+                        label_smoothing=0 \
+                        optimizer=Adam, \
+                        learning_rate_schedule="constant", \
+                        learning_rate_constant=6e-4, \
+                        learning_rate=6e-4, \
+                        optimizer_adam_epsilon=1e-9,
+                        optimizer_adam_beta1=0.9,
+                        optimizer_adam_beta2=0.995'
 export TPU_NAME=actualmathstpu	# different for each run
 export STORAGE_BUCKET=gs://mathsreasoning
+export MODEL_TAG=mds_paper_settings
+export MODEL_TAG=${MODEL_TAG}-$(date +%F)
 
 export DATA_DIR=${STORAGE_BUCKET}/t2t-data
 export TMP_DIR=${STORAGE_BUCKET}/t2t_datagen
-export TRAIN_DIR=${STORAGE_BUCKET}/t2t_train/$PROBLEM/$MODEL-$HPARAMS
+export TRAIN_DIR=${STORAGE_BUCKET}/t2t_train/$PROBLEM/$MODEL-$MODEL_TAG
 
 # all the correct directories already exist. no need to mkdir
 
@@ -25,10 +42,10 @@ t2t-trainer \
   --problem=$PROBLEM \
   --model=$MODEL \
   --hparams_set=$HPARAMS \
-  --hparams='batch_size=1024, clip_grad_norm=0.1, optimizer=Adam' \
+  --hparams=
   --output_dir=$TRAIN_DIR \
   --use_tpu=True \
   --cloud_tpu_name=${TPU_NAME} \
   --train_steps=600000 \
   --eval_steps=3 \
-  --save_checkpoints_secs=1800
+  --save_checkpoints_secs=3600
